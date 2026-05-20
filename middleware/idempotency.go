38fab443358c20767ee5eea81c6f900e65cc5397
EfoJensen <Jensena231@gmail.com>
2026-05-20 18:30:33 +0000

    added quick return on error in middleware

--- middleware/idempotency.go
+++ middleware/idempotency.go
@@ -2,12 +2,20 @@ package middleware
 
 import (
 	"context"
+	"errors"
 	"net/http"
+
+	"github.com/Efojensen/Idempotency-Gateway/utils"
 )
 
 func CheckIdempotencyKey(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
 		idempotencyKey := r.Header.Get("Idempotency-Key")
+
+		if idempotencyKey == "" {
+			utils.WriteErrorResponse(w, http.StatusBadRequest, errors.New("missing idempotency key"))
+			return
+		}
 
 		ctx := context.WithValue(r.Context(), "idempotencyKey", idempotencyKey)
 		next.ServeHTTP(w, r.WithContext(ctx))
